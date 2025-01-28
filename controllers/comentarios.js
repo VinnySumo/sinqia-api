a6a6308a315c03a669b660a6fd0e3d20a0676e64
Vynicios Raphael Silva de Souza <vinnyrafael015@hotmail.com>
2025-01-28 19:51:37 -0300

    testando as api

--- controllers/comentarios.js
+++ controllers/comentarios.js
@@ -1,26 +1,33 @@
 const db = require('../database/connection');
 
 module.exports = {
-    async listarComentarios(request, response) {
-        try {
-            const sql = 'SELECT * FROM tipo_endereco;';
-            const tiposEndereco = await db.query(sql);
-            const nItens = tiposEndereco[0].length;
-
-            return response.status(200).json({
-                sucesso: true,
-                mensagem: 'Lista de Tipos de Endereço.',
-                dados: tiposEndereco[0],
-                nItens
-            });
-        } catch (error) {
-            return response.status(500).json({
-                sucesso: false,
-                mensagem: 'Erro na requisição.',
-                dados: error.message
-            });
-        }
-    },
+ async listarComentarios(request, response) {
+         try {
+           
+           // Chama a função de conexão para obter a pool
+           const pool = await db.connect();  
+     
+           // Realiza a consulta SQL
+           const resultado = await pool.request().query('SELECT * FROM comentarios');
+     
+           // Resultado da consulta
+           const comentario = resultado.recordset;  
+           const nItens = comentario.length;
+     
+           return response.status(200).json({
+             sucesso: true,
+             mensagem: "Lista de comentarios do pontos turisticos.",
+             dados: comentario,
+             nItens,
+           });
+         } catch (error) {
+           return response.status(500).json({
+             sucesso: false,
+             mensagem: "Erro na requisição.",
+             dados: error.message,
+           });
+         }
+       },
 
     async cadastrarTipoEndereco(request, response) {
         try {
